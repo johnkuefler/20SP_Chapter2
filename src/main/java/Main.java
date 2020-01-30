948b842c16dee6f3b62262d22a0b53670d227ad4
WDAGUtilityAccount <WDAGUtilityAccount@d8d70816-1031-43fb-bb50-c25d2432f518.mshome.net>
2020-01-30 09:05:27 -0600

    added main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,37 @@
  * @author WDAGUtilityAccount
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        System.out.println("Class1:\t \"Intermediate Programming\"");
+        System.out.println("Class2:\t Systems Analysis");
+
+        String outputText = "This will not \n";
+        
+        
+        
+        System.out.print(outputText + " Be printed on one line " + 1000);
+
+        System.out.println("Hello there"
+                + "how are you"
+                + "etc");
+        
+        
+        int inches = 12;
+        double temperatureInF = 10.5;
+        byte myNumber;
+        myNumber = 2;
+        
+        boolean itsWarmOut = false;
+        
+        char firstLetterOfMyFirstName = 'J';
+        String firstLetterOfMyLastName = "K";
+        
+        
+            int month, days;
+            month = 2;
+            days = 28;
+            System.out.println("Month " + month + " has " +
+                    days + " Days.");
+    }
 }
